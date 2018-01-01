799db2d921c26606fac5d768efd83d1c0397cdf2
Kaplaugher <kaplaugher94@gmail.com>
2018-01-01 14:11:35 -0500

    finshed

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
 import { Form, FormControl, Button } from 'react-bootstrap';
+import AgeStats from './AgeStats';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newDate: '',
+      birthday: '1992-06-21',
+      showStats: false
+    }
+  }
+
+  changeBirthday() {
+    console.log(this.state);
+    this.setState({ 
+      birthday: this.state.newDate,
+      showStats: true });
+  }
+
   render() {
     return (
       <div className='App'>
         <Form inline>
           <h2>Input Your Birthday!</h2>
-          <FormControl type='date'>
+          <FormControl  
+            type='date'
+            onChange={ event => this.setState({ newDate: event.target.value })}
+            >
           </FormControl>
           {' '}
-          <Button>
+          <Button onClick={() => this.changeBirthday()}>
             Submit
           </Button>
         </Form>
+        {
+          this.state.showStats ?
+            <div className='fade age-stats'>
+              <AgeStats date={this.state.birthday}/>
+            </div> 
+          :
+            <div></div>
+        }
       </div>
     );
   }
